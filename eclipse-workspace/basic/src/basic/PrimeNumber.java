4f6570152bbd141b30607fa32a69b3fdf8151fcc
.BALA <balaakashb09@gmail.com.com>
2024-03-08 14:45:03 +0530

    java program

---
+++ eclipse-workspace/basic/src/basic/PrimeNumber.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 17; // Change num as needed
+        boolean isPrime = true;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(num + " is prime? " + isPrime);
+    }
+}
